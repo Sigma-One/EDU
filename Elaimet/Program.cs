4f06b8fd156cd955fbf3b19e0e6f45ff73b1d85f
Sigma-One <Sigma1@tuta.io>
2021-08-11 13:46:13 +0300

    Englishified

--- Elaimet/Program.cs
+++ Elaimet/Program.cs
@@ -1,32 +1,29 @@
 ﻿using System;
 
 /**
-  
    Muista kommentoida toiminnalliset funktiot!
 
    https://msdn.microsoft.com/en-us/library/5ast78ax.aspx
 
-    1. Luokassa Hevonen ei ole kuin yhden tyyppinen konstruktori,  parametriton konstruktori. Tutki luokkaa.
-    2. Luo Hevonen -luokasta olio Main -metodissa
+    1. Luokassa Horse ei ole kuin yhden tyyppinen konstruktori,  parametriton konstruktori. Tutki luokkaa.
+    2. Luo Horse -luokasta olio Main -metodissa
 	3. Lisää rivi missä Tulostat olion nimi ja paino näytölle.
 	4. Lisää koodiin rivit missä annetaan luodulle oliolle nimi ja paino.
-    5. Tulosta olion tiedot nyt näytölle.
-  
-   
+    5. Tulosta olion tiedot nyt näytölle. 
 **/
 
 
 namespace Elaimet {
     internal class Program {
         private static void Main(string[] args) {
-            Hevonen hevonen = new Hevonen();
+            Horse horse = new Horse();
 
-            Console.WriteLine("nimi  : " + hevonen.Nimi + "\npaino : " + hevonen.Paino + "\n");
+            Console.WriteLine("name  : " + horse.Name + "\nweight : " + horse.Weight + "\n");
 
-            hevonen.Nimi = "Histamiini";
-            hevonen.Paino = 89;
+            horse.Name = "Histamiini";
+            horse.Weight = 89;
 
-            Console.WriteLine("nimi  : " + hevonen.Nimi + "\npaino : " + hevonen.Paino);
+            Console.WriteLine("name  : " + horse.Name + "\nweight : " + horse.Weight);
         }
     }
 }
